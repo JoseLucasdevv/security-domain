d079dd382ac4cf22f7b5683bfc56dd235aecaaa9
JoseLucasdevv <joselucas.dev@gmail.com>
2025-01-17 14:39:09 -0300

    new package Api with basic config userResource

---
+++ src/main/java/app/security/api/UserResource.java
@@ -0,0 +1,29 @@
+package app.security.api;
+
+import app.security.domain.User;
+import app.security.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
