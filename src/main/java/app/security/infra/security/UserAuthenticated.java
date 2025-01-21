2e9381ac8e47099312e7d684dcb045043de87c45
JoseLucasdevv <joselucas.dev@gmail.com>
2025-01-21 13:41:15 -0300

    feat:UserAuthenticated implements userDetails by spring security

---
+++ src/main/java/app/security/infra/security/UserAuthenticated.java
@@ -0,0 +1,55 @@
+package app.security.infra.security;
+
+import app.security.Enum.TypeRole;
+import app.security.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserAuthenticated implements UserDetails {
+    private final User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<TypeRole> roles = user.getRoles().stream().map(r -> r.getName()).toList();
+        if(roles.contains(TypeRole.ADMIN)){
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
