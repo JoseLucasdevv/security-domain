22c62a886ba58d8ac93fa793a586738b1d9bbdef
JoseLucasdevv <joselucas.dev@gmail.com>
2025-01-21 13:29:40 -0300

    refactor:change Type of name role

--- src/main/java/app/security/domain/Role.java
+++ src/main/java/app/security/domain/Role.java
@@ -1,5 +1,6 @@
 package app.security.domain;
 
+import app.security.Enum.TypeRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,5 +16,5 @@ import lombok.NoArgsConstructor;
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
+    private TypeRole name;
 }
