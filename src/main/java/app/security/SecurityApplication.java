cb293b4a02fa65f6a0a078b6586fb7464dcd1ad4
JoseLucasdevv <joselucas.dev@gmail.com>
2025-01-21 13:31:06 -0300

    delete: remove raw DB script initialize

--- src/main/java/app/security/SecurityApplication.java
+++ src/main/java/app/security/SecurityApplication.java
@@ -1,13 +1,23 @@
 package app.security;
 
+import app.security.domain.Role;
+import app.security.domain.User;
+import app.security.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+
+
 	}
+
 
 }
