4d266d9b1e6662ff06111cc299dd47241f4b5a8f
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-06 10:45:08 -0300

    refactor:remove useless imports.

--- src/main/java/app/security/SecurityApplication.java
+++ src/main/java/app/security/SecurityApplication.java
@@ -1,14 +1,7 @@
 package app.security;
 
-import app.security.domain.Role;
-import app.security.domain.User;
-import app.security.services.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -19,5 +12,5 @@ public class SecurityApplication {
 
 	}
 
-	
+
 }
