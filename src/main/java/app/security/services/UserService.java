4a3ce6b4f79e3692fa05190d1065363d1e7f38d1
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-06 10:42:09 -0300

    refactor: remove useless methods

--- src/main/java/app/security/services/UserService.java
+++ src/main/java/app/security/services/UserService.java
@@ -1,7 +1,5 @@
 package app.security.services;
 
-import app.security.Enum.TypeRole;
-import app.security.domain.Role;
 import app.security.domain.User;
 
 import java.util.List;
@@ -9,8 +7,6 @@ import java.util.List;
 public interface UserService {
 
     User saveUser(User user);
-    Role saveRole(Role role);
-    void addRoleToUser(String username, TypeRole roleName);
     User getUser(String username);
     List<User> getUsers();
 
