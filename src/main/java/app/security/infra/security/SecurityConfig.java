67b73676a81d66aab4cf184946bad7a5a71a7c41
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-06 10:40:24 -0300

    feat:inset class by dependency injection and filter before

--- src/main/java/app/security/infra/security/SecurityConfig.java
+++ src/main/java/app/security/infra/security/SecurityConfig.java
@@ -1,27 +1,39 @@
 package app.security.infra.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+    @Autowired
+    FilterValidateJWT filterValidateJWT;
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf-> csrf.disable())
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("api/users")
-                        .authenticated()
-                        .anyRequest()
-                        .permitAll());
+                .authorizeHttpRequests(auth -> auth.
+                        requestMatchers(HttpMethod.POST,"api/register").permitAll().
+                        requestMatchers(HttpMethod.POST,"api/auth").permitAll()
+                        .requestMatchers(HttpMethod.GET,"api/users").hasRole("ADMIN")
+
+
+                ).addFilterBefore(filterValidateJWT, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
@@ -35,6 +47,10 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return  authenticationConfiguration.getAuthenticationManager();
+    }
 
 
 }
