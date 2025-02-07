7db6eecf3432c446504ccc9f76f4763748ebee46
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-07 16:47:20 -0300

    refactor:AuthResource

--- src/main/java/app/security/api/AuthResource.java
+++ src/main/java/app/security/api/AuthResource.java
@@ -1,63 +1,41 @@
 package app.security.api;
-
-import app.security.Enum.TypeRole;
-import app.security.domain.Role;
-import app.security.domain.User;
-import app.security.infra.security.UserAuthenticated;
 import app.security.repository.RoleRepository;
-import app.security.services.JwtService;
-import app.security.services.UserService;
+import app.security.services.AuthService;
 
+import app.security.types.AuthDTO;
+import app.security.types.RegisterDTO;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
 
 
 @RestController
 @RequestMapping("/api")
 public class AuthResource {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    JwtService jwtService;
-    @Autowired
-    UserService userService;
+@Autowired
+    AuthService authService;
     @Autowired
     private RoleRepository roleRepository;
 
     @PostMapping("/register")
-    public ResponseEntity registerResource(@RequestBody UserDTO form){
+    public ResponseEntity registerResource(@RequestBody RegisterDTO form){
 
+        authService.register(form);
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(form.password);
-        User persistUser = new User();
-        persistUser.setName(form.getName());
-        persistUser.setPassword(encryptedPassword);
-        persistUser.setUsername(form.getUsername());
-        Role roleUser = new Role();
-        roleUser.setName(TypeRole.USER);
-        roleRepository.save(roleUser);
-        persistUser.setRoles(List.of(roleUser));
-        userService.saveUser(persistUser);
 
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/auth")
-    public ResponseEntity authResource(@RequestBody Auth form){
+    public ResponseEntity authResource(@RequestBody AuthDTO form){
         try{
-            var usernamePassword = new UsernamePasswordAuthenticationToken(form.getUsername(), form.getPassword());
-            var auth = authenticationManager.authenticate(usernamePassword);
-
-            var token = jwtService.generateToken((UserAuthenticated) auth.getPrincipal());
+            var token = authService.auth(form);
             return ResponseEntity.ok(token);
         }catch(Exception e ){
             return ResponseEntity.badRequest().body(e);
@@ -68,16 +46,4 @@ public class AuthResource {
     }
 
 
-        @Data
-        static class UserDTO {
-            private String name;
-            private String username;
-            private String password;
-        }
-
-    @Data
-    static class Auth {
-        private String username;
-        private String password;
-    }
 }
