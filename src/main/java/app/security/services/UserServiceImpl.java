c36ae4ede7033bc93a4f789ae6eb016d94014d76
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-07 16:46:27 -0300

    refactor:remove useless methods in UserServiceImpl.java

--- src/main/java/app/security/services/UserServiceImpl.java
+++ src/main/java/app/security/services/UserServiceImpl.java
@@ -28,19 +28,6 @@ public class UserServiceImpl implements UserService {
         return this.userRepository.save(user);
     }
 
-    @Override
-    public Role saveRole(Role role) {
-        log.info("save new Role {} in database",role.getName());
-        return this.roleRepository.save(role);
-    }
-
-    @Override
-    public void addRoleToUser(String userName, TypeRole roleName) {
-        log.info("Add new Role {} to User {}",userName,roleName);
-    User user = this.userRepository.findByUsername(userName);
-    Role role = this.roleRepository.findByname(roleName);
-    user.getRoles().add(role);
-    }
 
     @Override
     public User getUser(String username) {
