5f33607fe86c9a922cd87215bca2d91b4ea374cf
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-08 17:28:49 -0300

    refactor:remove useless entryPoint saveUser

--- src/main/java/app/security/api/UserResource.java
+++ src/main/java/app/security/api/UserResource.java
@@ -1,17 +1,11 @@
 package app.security.api;
 
-import app.security.Enum.TypeRole;
-import app.security.domain.Role;
 import app.security.domain.User;
 import app.security.services.UserService;
-
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 import java.util.List;
 
@@ -19,26 +13,12 @@ import java.util.List;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserResource {
-
     private final UserService userService;
 
-
-
+    //Teacher Resource
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
-    @PostMapping("/user/save")
-    public ResponseEntity<User> saveUser(@RequestBody User user){
-         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
-        return ResponseEntity.created(uri).body(userService.saveUser(user));
-    }
-
-
-    @Data
-    class RoleToUserForm{
-        private String userName;
-        private TypeRole roleName;
-    }
 }
