da07f6a53e1aa6d601a918b3ab836bc9e98677d1
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-10 16:25:54 -0300

    feat:add users MapedBy User.java

--- src/main/java/app/security/domain/Role.java
+++ src/main/java/app/security/domain/Role.java
@@ -1,18 +1,25 @@
 package app.security.domain;
 
 import app.security.Enum.TypeRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Enumerated(EnumType.STRING)
     private TypeRole name;
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<User> users;
 }
