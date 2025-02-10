f89f6012283f25c7b0f6717a495545ec42b09b69
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-10 16:30:28 -0300

    feat:implements getUsers resource

--- src/main/java/app/security/api/UserTeacherResource.java
+++ src/main/java/app/security/api/UserTeacherResource.java
@@ -3,6 +3,8 @@ package app.security.api;
 import app.security.domain.User;
 import app.security.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +17,15 @@ public class UserTeacherResource {
     private final UserService userService;
 
     //Teacher Resource
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> getUsers(){
-        return ResponseEntity.ok().body(userService.getUsers());
+    @GetMapping("/teacher/users/{pageNumber}")
+    public ResponseEntity<List<User>> getUsers(@PathVariable int pageNumber){
+
+        return ResponseEntity.ok().body(userService.getUsers(pageNumber));
     }
+
+    @GetMapping("/teacher/user/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username){
+            return ResponseEntity.ok().body(userService.getUser(username));
+    }
+
 }
