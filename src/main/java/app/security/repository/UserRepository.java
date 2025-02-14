5db67dbbd95703a52f252f4d7ba18361693bc5ea
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-13 22:00:33 -0300

    feat:feat add getUserById and getAllWorkoutFromUser Querry

--- src/main/java/app/security/repository/UserRepository.java
+++ src/main/java/app/security/repository/UserRepository.java
@@ -2,6 +2,7 @@ package app.security.repository;
 
 import app.security.Enum.TypeRole;
 import app.security.domain.User;
+import app.security.domain.Workout;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,4 +18,11 @@ public interface UserRepository extends JpaRepository<User , Long> {
 
     @Query("SELECT u FROM User u WHERE u.role.name = :role_name")
     Page<User> getAllByRoleNameUser(@Param("role_name") TypeRole roleName,Pageable page);
+
+    @Query("SELECT u FROM User u WHERE u.role.name = :role_name AND u.id = :user_id")
+    User getUserById(@Param("role_name") TypeRole roleName ,@Param ("user_id") long id);
+
+    @Query("SELECT u.workout FROM User u WHERE u.role.name = :role_name AND u.username = :username")
+    List<Workout> getAllWorkoutFromUser(@Param("role_name")TypeRole roleName,String username);
+
 }
