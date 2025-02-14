d18270851499118d95b4d0dffbc17c27e1cf2ea7
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-13 22:01:14 -0300

    feat:add methods teacherUseCasesImpl

--- src/main/java/app/security/services/UserServiceImpl.java
+++ src/main/java/app/security/services/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package app.security.services;
 
-
 import app.security.Enum.TypeRole;
 import app.security.domain.User;
+import app.security.domain.Workout;
 import app.security.repository.UserRepository;
+import app.security.repository.WorkoutRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,14 +14,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class UserServiceImpl implements UserService {
-
+    private final WorkoutRepository workoutRepository;
     private final UserRepository userRepository;
 
 
@@ -27,6 +30,13 @@ public class UserServiceImpl implements UserService {
     public User saveUser(User user) {
         log.info("save new user {} in database",user.getName());
         return this.userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        var user = this.userRepository.getUserById(TypeRole.USER,id);
+        log.info("get user {}",user.getUsername());
+        return user;
     }
 
 
@@ -38,9 +48,39 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getUsers(int pageNumber) {
-        Pageable page = PageRequest.of(pageNumber - 1 ,10);
+        Pageable page = PageRequest.of(pageNumber -1 ,10);
         log.info("get all Users");
 
-        return this.userRepository.getAllByRoleNameUser(TypeRole.USER,page).stream().toList();
+        return this.userRepository.getAllByRoleNameUser(TypeRole.USER,page).stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Workout> getAllWorkoutFromUser(String username) {
+        log.info("get all workout from user");
+        return this.userRepository.getAllWorkoutFromUser(TypeRole.USER , username);
+    }
+
+    @Override
+    public Workout getSpecificWorkoutFromUser(String username, Long workoutId) {
+        List<Workout> listWorkout = this.userRepository.getAllWorkoutFromUser(TypeRole.USER,username);
+
+        return listWorkout.stream().filter(w -> workoutId.equals(w.getId())).findFirst().orElse(null);
+        // return this.workoutRepository.findById(workoutId).orElseThrow(() ->  new EntityNotFoundException("Workout not found"));
+
+    }
+
+    @Override
+    public Void deleteWorkoutById(String username, Long workoutId) {
+        return null;
+    }
+
+    @Override
+    public User createWorkout(String username, Workout workout) {
+        return null;
+    }
+
+    @Override
+    public User updateWorkout(String username, Workout workout) {
+        return null;
     }
 }
