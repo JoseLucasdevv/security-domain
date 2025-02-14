4e2ff4b708f26379f3c39238c82e8035ba74aaf8
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-13 22:00:54 -0300

    feat:add methods teacherUseCases

--- src/main/java/app/security/services/UserService.java
+++ src/main/java/app/security/services/UserService.java
@@ -1,13 +1,24 @@
 package app.security.services;
 
 import app.security.domain.User;
+import app.security.domain.Workout;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
-
+    //userConsumer
     User saveUser(User user);
+
+//    userTeacher
+    User getUserById(Long id);
     User getUser(String username);
-    List<User> getUsers();
+    List<User> getUsers(int pageNumber);
+    List<Workout> getAllWorkoutFromUser(String username);
+    Workout getSpecificWorkoutFromUser(String username, Long workoutId);
+    Void deleteWorkoutById(String username, Long workoutId);
+    User createWorkout(String username, Workout workout);
+    User updateWorkout(String username, Workout workout);
+
 
 }
