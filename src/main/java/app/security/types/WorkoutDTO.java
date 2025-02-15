d62b10bf051d56bf3d8a8c953e7a4446ae91c0fb
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-15 15:22:23 -0300

    refactor:change User Param to Generic <T>

--- src/main/java/app/security/types/WorkoutDTO.java
+++ src/main/java/app/security/types/WorkoutDTO.java
@@ -7,7 +7,7 @@ import app.security.domain.User;
 import java.time.LocalDateTime;
 
 
-public record WorkoutDTO(Long id, String name, String series, WeekDay weekday, String description, String nameOfTeacher,
-                         User user,
+public record WorkoutDTO<T>(Long id, String name, String series, WeekDay weekday, String description, String nameOfTeacher,
+                         T user,
                          LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
