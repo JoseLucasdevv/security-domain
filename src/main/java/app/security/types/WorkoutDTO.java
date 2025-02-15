87b2ceef1f3ccfbdfb0eeaad75a8907f8e3381ee
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-15 13:37:13 -0300

    refactor:change types from parameters

--- src/main/java/app/security/types/WorkoutDTO.java
+++ src/main/java/app/security/types/WorkoutDTO.java
@@ -3,9 +3,11 @@ package app.security.types;
 import app.security.Enum.WeekDay;
 import app.security.domain.User;
 
-import java.util.Date;
+
+import java.time.LocalDateTime;
+
 
 public record WorkoutDTO(Long id, String name, String series, WeekDay weekday, String description, String nameOfTeacher,
                          User user,
-                         Date createdAt, Date updatedAt) {
+                         LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
