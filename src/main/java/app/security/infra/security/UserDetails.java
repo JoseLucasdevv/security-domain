04e62f1faabd1b562c995cbb903e908c47d4b252
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-15 17:45:50 -0300

    refactor: change userService injection to userRepository injection

--- src/main/java/app/security/infra/security/UserDetails.java
+++ src/main/java/app/security/infra/security/UserDetails.java
@@ -2,6 +2,7 @@ package app.security.infra.security;
 
 import app.security.domain.User;
 
+import app.security.repository.UserRepository;
 import app.security.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,11 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetails implements UserDetailsService {
-private final UserService userService;
+
+    private final UserRepository userRepository;
 
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getUser(username);
+
+        User user = this.userRepository.findByUsername(username);
+
+
         if(user == null){
             throw new UsernameNotFoundException("user not found");
         }
