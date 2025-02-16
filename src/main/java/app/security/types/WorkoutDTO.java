08dd875bfbb504e7abbb5011afa87d8d1f8f1e0c
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-16 14:26:33 -0300

    feat:add new Attr muscularGroup

--- src/main/java/app/security/types/WorkoutDTO.java
+++ src/main/java/app/security/types/WorkoutDTO.java
@@ -1,10 +1,11 @@
 package app.security.types;
 
+import app.security.Enum.MuscularGroup;
 import app.security.Enum.WeekDay;
 import java.time.LocalDateTime;
 
 
-public record WorkoutDTO<T>(Long id, String name, String series, WeekDay weekday, String description, String nameOfTeacher,
-                         T user,
-                         LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record WorkoutDTO<T>(Long id, String name, String series, WeekDay weekday, MuscularGroup musculargroup, String description, String nameOfTeacher,
+                            T user,
+                            LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
