ed08fd52241e05db56876cb98765a3a27036018d
JoseLucasDevv <joselucas.dev@gmail.com>
2025-02-20 09:16:28 -0300

    feat: add UserConsumerResource.java to get AllWorkouts

---
+++ src/main/java/app/security/api/UserConsumerResource.java
@@ -0,0 +1,31 @@
+package app.security.api;
+
+import app.security.services.UserService;
+import app.security.types.WorkoutDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserConsumerResource {
+    private final UserService userSerivce;
+
+    @GetMapping("/user/resource/workouts")
+    public ResponseEntity<List<WorkoutDTO<String>>> getMyWorkout(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+
+        return ResponseEntity.ok(userSerivce.getAllWorkoutFromUser(username));
+
+    }
+
+
+}
