78c04dbd3ecb81919bfc625898f738d651a9d6b5
JoseLucasdevv <joselucas.dev@gmail.com>
2025-02-20 15:28:47 -0300

    fix:Correct the name of Parameter to camelCase

--- src/main/java/app/security/types/WorkoutDTO.java
+++ src/main/java/app/security/types/WorkoutDTO.java
@@ -5,7 +5,7 @@ import app.security.Enum.WeekDay;
 import java.time.LocalDateTime;
 
 
-public record WorkoutDTO<T>(Long id, String name, String series, WeekDay weekday, MuscularGroup musculargroup, String description, String nameOfTeacher,
+public record WorkoutDTO<T>(Long id, String name, String series, WeekDay weekday, MuscularGroup muscularGroup, String description, String nameOfTeacher,
                             T user,
                             LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
