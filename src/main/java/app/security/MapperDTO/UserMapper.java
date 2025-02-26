a457639bed93be608d6c2647a06b34156c13564a
JoseLucasdevv <joselucas.dev@gmail.com>
2025-02-26 17:14:38 -0300

    feat:create dtoTOUser

--- src/main/java/app/security/MapperDTO/UserMapper.java
+++ src/main/java/app/security/MapperDTO/UserMapper.java
@@ -2,16 +2,29 @@ package app.security.MapperDTO;
 
 import app.security.domain.User;
 import app.security.types.UserDTO;
+import app.security.types.UserDTOIn;
 
 public class UserMapper {
 
 
     public static UserDTO UserToDTO(User user){
-        return new UserDTO(user.getId(),user.getName(), user.getUsername(), user.getPassword(),user.getRole(),user.getWorkout().stream().toList());
+        return new UserDTO(user.getId(),user.getName(), user.getUsername(),user.getRole().getName(),user.getWorkout().stream().toList());
     }
 
-    public static User DtoToUser(UserDTO userDTO){
-        return new User(userDTO.id(),userDTO.name(),userDTO.username(),userDTO.password(),userDTO.role(),userDTO.workout());
+
+    public static User DtoToUser(UserDTOIn user){
+    User userEntity = new User();
+    userEntity.setName(user.name());
+    userEntity.setUsername(user.username());
+    userEntity.setPassword(user.password());
+
+
+
+
+    return userEntity;
+
+
+
     }
 
 }
