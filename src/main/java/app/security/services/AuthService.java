a124ba2eb2580fdfba7b0b1e59018a77aedb0c29
JoseLucasdevv <joselucas.dev@gmail.com>
2025-02-26 17:09:56 -0300

    refactor:add new parameter role

--- src/main/java/app/security/services/AuthService.java
+++ src/main/java/app/security/services/AuthService.java
@@ -1,10 +1,11 @@
 package app.security.services;
 
+import app.security.Enum.TypeRole;
 import app.security.types.AuthDTO;
 import app.security.types.RegisterDTO;
 
 public interface AuthService {
-     void register(RegisterDTO form);
+     void register(RegisterDTO form, TypeRole role);
 
      String auth(AuthDTO form);
 }
