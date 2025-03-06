e18a31f6094b1b5f0a0585d19e062e6763ac2493
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-06 11:20:16 -0300

    feat:create new methods to handle exceptions

--- src/main/java/app/security/exceptions/GeneralExceptionHandler.java
+++ src/main/java/app/security/exceptions/GeneralExceptionHandler.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,6 +30,23 @@ public class GeneralExceptionHandler {
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, handleErrorResponse>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, handleErrorResponse> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((err) -> {
+            errors.put("error",new handleErrorResponse(((FieldError) err).getField(),err.getDefaultMessage()));
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, handleErrorResponse>> handleValidationExceptions(BadCredentialsException ex) {
+        Map<String, handleErrorResponse> errors = new HashMap<>();
+
+        errors.put("error",new handleErrorResponse("username or password",ex.getMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 
 
     @ExceptionHandler(Exception.class)
