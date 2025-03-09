f934c50561b38c8a2c0c463e9890c806dbfb08e6
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-09 09:37:50 -0300

    feat:createMethod abstract extractExpiresAt

--- src/main/java/app/security/services/JwtService.java
+++ src/main/java/app/security/services/JwtService.java
@@ -2,7 +2,10 @@ package app.security.services;
 
 import app.security.infra.security.UserAuthenticated;
 
+import java.time.Instant;
+
 public interface JwtService {
     String generateToken(UserAuthenticated user);
     String validateToken(String token);
+    Instant extractExpiresAt(String token);
 }
