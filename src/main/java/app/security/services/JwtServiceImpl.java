0c68ec203710ee0719d633077b7b654cdabb7493
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-09 09:38:31 -0300

    feat:createMethod Implement extractExpiresAt

--- src/main/java/app/security/services/JwtServiceImpl.java
+++ src/main/java/app/security/services/JwtServiceImpl.java
@@ -1,13 +1,17 @@
 package app.security.services;
 
+
 import app.security.infra.security.UserAuthenticated;
+import app.security.utils.GenerateExpirationDate;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Service;
+import java.time.*;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+
 @Service
 public class JwtServiceImpl implements JwtService{
     String secretKey = "123791823791283sdakspoKOPASKAKSO";
@@ -15,26 +19,33 @@ public class JwtServiceImpl implements JwtService{
 
     @Override
     public String generateToken(UserAuthenticated user){
-
         try{
             Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
 
             return JWT.create()
                     .withIssuer("security-domain")
                     .withSubject(user.getUsername())
-                    .withExpiresAt(genExpirationDate())
+                    .withExpiresAt(GenerateExpirationDate.genExpirationDate(1L))
                     .sign(algorithm);
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (JWTCreationException e) {
+            throw new JWTCreationException("fail to create AccessToken:" + e.getMessage(),e.getCause());
+        }
+
+    }
+
+    @Override
+    public Instant extractExpiresAt(String token){
+        try{
+            return JWT.decode(token).getExpiresAt().toInstant();
+        } catch (JWTDecodeException e) {
+            throw new JWTDecodeException("cannot decode token");
         }
 
     }
 
     @Override
     public String validateToken(String token) {
-
-
         try{
             Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
             return JWT.require(algorithm)
@@ -42,13 +53,12 @@ public class JwtServiceImpl implements JwtService{
                     .build()
                     .verify(token)
                     .getSubject();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        }catch(JWTVerificationException e){
+            throw new JWTVerificationException("cannot verify token:" + e.getMessage());
         }
 
+
     }
 
-    private Instant genExpirationDate(){
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-    }
+
 }
