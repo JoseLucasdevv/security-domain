9665418f13179ce056fade5ab93a1bd14b9909c3
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-09 09:37:30 -0300

    feat:create global exception to handle expiredToken

--- src/main/java/app/security/exceptions/GeneralExceptionHandler.java
+++ src/main/java/app/security/exceptions/GeneralExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.security.sasl.AuthenticationException;
 
 
 @ControllerAdvice
@@ -47,6 +48,10 @@ public class GeneralExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleExpiredToken(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 
 
     @ExceptionHandler(Exception.class)
