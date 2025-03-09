e550c7187acc9d3fcd546feff3ec0c0a5ee72080
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-09 09:31:58 -0300

    feat:create DTO refreshTokenIn

---
+++ src/main/java/app/security/types/RefreshTokenIn.java
@@ -0,0 +1,7 @@
+package app.security.types;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RefreshTokenIn(@NotBlank(message = "cannot be blank") @NotNull(message = "cannot be Null") String refreshToken) {
+}
