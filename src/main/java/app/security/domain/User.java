d516b5835ddc15a16ce51b290946c0e1d33f4dee
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-10 14:30:47 -0300

    feat:create TokenBlackListCollection attr

--- src/main/java/app/security/domain/User.java
+++ src/main/java/app/security/domain/User.java
@@ -1,16 +1,14 @@
 package app.security.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.sql.Ref;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -21,6 +19,8 @@ public class User {
     private Long id;
     private String name;
     private String username;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<TokenBlackList> accessTokenBlackList = new HashSet<>();
     private String password;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id")
