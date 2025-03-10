071d801af86470da71cc06e21923681bca23ed73
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-10 14:29:13 -0300

    feat:create logOut method AuthService

--- src/main/java/app/security/services/AuthService.java
+++ src/main/java/app/security/services/AuthService.java
@@ -2,6 +2,7 @@ package app.security.services;
 
 import app.security.Enum.TypeRole;
 import app.security.dto.AuthDTO;
+import app.security.dto.LogOutIn;
 import app.security.dto.RegisterDTO;
 import app.security.dto.ResponseAuthentication;
 
@@ -10,4 +11,6 @@ public interface AuthService {
      void register(RegisterDTO form, TypeRole role);
 
     ResponseAuthentication auth(AuthDTO form);
+
+    void logOut(LogOutIn logOut);
 }
