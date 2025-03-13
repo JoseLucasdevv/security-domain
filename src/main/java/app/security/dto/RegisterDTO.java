beb3420d52a5117f9c6fb782f16a87f25e665859
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-13 12:06:30 -0300

    feat:add validation and new Input value Email

--- src/main/java/app/security/dto/RegisterDTO.java
+++ src/main/java/app/security/dto/RegisterDTO.java
@@ -1,15 +1,12 @@
 package app.security.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record RegisterDTO(@NotNull(message = "cannot be null") @NotBlank(message = "cannot be blank") @Size(min= 3,max = 55,message = "name should have between 3 and 55 characters")String name,
                           @NotNull(message = "cannot be null") @NotBlank(message = "cannot be blank") @Size(min= 3,max = 55,message = "username should have between 3 and 55 characters")String username,
-                          @NotNull(message = "cannot be null") @NotBlank(message = "cannot be blank")@Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,}$",message = "your password should have at least 8 characters One UpperCase letter and One Especial Character")
-                          String password,
+                          @NotNull(message = "cannot be null") @NotBlank(message = "cannot be blank") @Email(message = "email not valid") String email,
+                          @NotNull(message = "cannot be null") @NotBlank(message = "cannot be blank")@Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,}$",message = "your password should have at least 8 characters One UpperCase letter and One Especial Character") String password,
                           String confirmPassword)
 {
 }
