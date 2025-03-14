bff51f0b8998616142fd0663bd71531af7b69b1c
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-13 21:02:04 -0300

    feat: create EmailServiceImpl with method VerifyTokenEmailConfirmation implemented

---
+++ src/main/java/app/security/services/EmailServiceImpl.java
@@ -0,0 +1,30 @@
+package app.security.services;
+
+import app.security.domain.User;
+import app.security.exceptions.Exception;
+import app.security.repository.EmailConfirmationTokenRepository;
+import app.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    @Autowired
+    private EmailConfirmationTokenRepository emailConfirmationTokenRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void verifyTokenEmailConfirmation(String token) {
+        var tokenEmailConfirmation = emailConfirmationTokenRepository
+                .findEmailConfirmationTokenByToken(token)
+                .orElseThrow(()->new Exception("the token is Invalid"));
+        if(tokenEmailConfirmation.getExpiresAt().isBefore(Instant.now())) throw new Exception("the token was expired");
+
+        User user =tokenEmailConfirmation.getUser();
+        user.setEmailConfirmed(true);
+        this.userRepository.save(user);
+
+    }
+}
