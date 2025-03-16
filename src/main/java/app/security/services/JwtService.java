5ccae2564801868f5c9a9127923e24bce8ed6ca1
JoseLucasdevv <joselucas.dev@gmail.com>
2025-03-16 09:04:14 -0300

    feat:create new method jwtService

--- src/main/java/app/security/services/JwtService.java
+++ src/main/java/app/security/services/JwtService.java
@@ -1,11 +1,14 @@
 package app.security.services;
 
 import app.security.infra.security.UserAuthenticated;
+import com.auth0.jwt.interfaces.Claim;
 
 import java.time.Instant;
+import java.util.Map;
 
 public interface JwtService {
     String generateToken(UserAuthenticated user);
     String validateToken(String token);
     Instant extractExpiresAt(String token);
+    Map<String, Claim> extractClaims(String token);
 }
