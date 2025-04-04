7e5e1eead18e6b801503af38934d72c0e8f08b90
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-04 12:09:49 -0300

    feat:create new method for sendEmailService interface.

--- src/main/java/app/security/mailProvider/SendEmailService.java
+++ src/main/java/app/security/mailProvider/SendEmailService.java
@@ -3,5 +3,6 @@ package app.security.mailProvider;
 public interface SendEmailService {
     void sendConfirmationLink(String token);
     void sendForgotPasswordLink(String token);
+    void SendCodeVerifyEmail(String code);
 
 }
