49e154fb4243881460d3fb316fb06d65e2439b90
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-04 16:19:08 -0300

    feat:add new resource to deleteAccount from user.

--- src/main/java/app/security/api/UserConsumerResource.java
+++ src/main/java/app/security/api/UserConsumerResource.java
@@ -2,11 +2,13 @@ package app.security.api;
 
 
 import app.security.dto.WorkoutDTO;
+import app.security.services.UserService;
 import app.security.services.WorkoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +19,27 @@ import java.util.List;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserConsumerResource {
+    private final UserService userService;
     private final WorkoutService workoutService;
-    @GetMapping("/user/resource/workouts")
+    @GetMapping("/user")
     public ResponseEntity<List<WorkoutDTO<String>>> getMyWorkout(){
         try{
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
+            String username = auth.getName();
 
-        return ResponseEntity.ok(workoutService.getAllWorkoutFromUser(username));
+            return ResponseEntity.ok(workoutService.getAllWorkoutFromUser(username));
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity deleteMyAccount(){
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String username = auth.getName();
+            this.userService.deleteUserByUsername(username);
+            return ResponseEntity.ok().build();
+
     }
 
 
