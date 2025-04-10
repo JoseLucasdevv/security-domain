e7e6b6a80227fcdb5bffd2d1d6076d4c498b91ae
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:49:05 -0300

    feat: catch exception to handle jwtVerification.

--- src/main/java/app/security/infra/security/FilterValidateJWT.java
+++ src/main/java/app/security/infra/security/FilterValidateJWT.java
@@ -1,10 +1,9 @@
 package app.security.infra.security;
 
-
-
 import app.security.repository.TokenBlackListRepository;
 import app.security.repository.UserRepository;
 import app.security.services.JwtService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class FilterValidateJWT extends OncePerRequestFilter {
@@ -32,16 +32,20 @@ public class FilterValidateJWT extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var auth = request.getHeader("Authorization");
         var token = getToken(auth);
-    if(token != null){
+        try{
+    if(token != null && !token.isBlank()){
         var userName = jwtService.validateToken(token);
         var extractClaims = jwtService.extractClaims(token);
         var emailConfirmed = extractClaims.get("Email-Confirmed").asBoolean();
+        UUID uid = UUID.fromString(extractClaims.get("uid").asString());
         request.setAttribute("Email-Confirmed",emailConfirmed);
+        request.setAttribute("uid",uid);
 
         var userFromRepository = this.userRepository.findByUsername(userName);
         tokenBlackListRepository.findByTokenBlackList(token).ifPresent(t-> {
         if(userFromRepository.getAccessTokenBlackList().contains(t)){
             new ServletException("this token in blackList");
+            return;
         };
     });
 
@@ -50,7 +54,15 @@ public class FilterValidateJWT extends OncePerRequestFilter {
     var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
     authentication.setDetails(extractClaims);
     SecurityContextHolder.getContext().setAuthentication(authentication);
-}
+
+}} catch (JWTVerificationException ex) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            String errorMessage = ex.getMessage();
+            String jsonResponse = String.format("{\"error\": \"%s\"}", errorMessage);
+            response.getWriter().write(jsonResponse);
+            return;
+        }
         filterChain.doFilter(request, response);
     }
 
