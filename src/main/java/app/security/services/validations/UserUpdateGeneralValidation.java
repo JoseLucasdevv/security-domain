18483b6df1524240ce48ae4a10d68e31f6c6aa5e
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:44:37 -0300

    feat: create  UserUpdateGeneralValidation.java

---
+++ src/main/java/app/security/services/validations/UserUpdateGeneralValidation.java
@@ -0,0 +1,114 @@
+package app.security.services.validations;
+
+import app.security.domain.User;
+import app.security.dto.UserUpdateDTO;
+import app.security.exceptions.Exception;
+import app.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+    @Service
+    @RequiredArgsConstructor
+    public class UserUpdateGeneralValidation {
+        private final PasswordEncoder passwordEncoder;
+        public final HashMap<String, String> resultValidation = new HashMap<String, String>();
+        private final UserRepository userRepository;
+        private boolean validateUpdate = false;
+        public ResponseUserUpdateValidation validationUpdate(User userAlreadyExist, UserUpdateDTO userUpdateValidation){
+            resultValidation.clear();
+            this.validateName(userAlreadyExist,userUpdateValidation.name());
+            this.validateUsername(userAlreadyExist,userUpdateValidation.username());
+            this.validatePassword(userAlreadyExist,userUpdateValidation.oldPassword(),userUpdateValidation.newPassword());
+
+            if(!this.validateUpdate) new Exception(new ResponseUserUpdateValidation(userAlreadyExist,this.resultValidation).toString(),HttpStatus.BAD_REQUEST);
+
+            userAlreadyExist.setUpdatedAt(LocalDateTime.now());
+            resultValidation.put("updated",userAlreadyExist.getUpdatedAt().toString());
+            return new ResponseUserUpdateValidation(userAlreadyExist,this.resultValidation);
+        }
+
+        private void validateName(User userAlreadyExist, String name){
+            if(name != null && !name.isBlank() && name.length() >= 3 && name.length() <= 55){
+                userAlreadyExist.setName(name.trim());
+                resultValidation.put("fieldName","Update Success");
+                this.validateUpdate = true;
+                return;
+
+            }
+            resultValidation.put("fieldName","check the minimum requirements to update the FieldName");
+        }
+
+        private void validateUsername(User userAlreadyExist,String username) {
+            if(username != null && !username.isBlank() && username.length() >= 3 && username.length() <= 55){
+
+                if(this.userRepository.findByUsername(username.trim()) != null) {
+                    resultValidation.put("fieldUsername","this username isn't allowed");
+                    return;
+                }
+
+                userAlreadyExist.setUsername(username.trim());
+                resultValidation.put("fieldUsername","Update Success");
+                this.validateUpdate = true;
+                return;
+            }
+            resultValidation.put("fieldUsername","check the minimum requirements to update the FieldUsername");
+        }
+
+        private void validatePassword(User userAlreadyExist, String oldPassword,String newPassword){
+
+            if(newPassword != null && !newPassword.isBlank() && oldPassword != null && !oldPassword.isBlank()){
+                boolean currPasswordValidation = this.passwordEncoder.matches(oldPassword,userAlreadyExist.getPassword());
+                System.out.println("value of matches from oldPassword" + currPasswordValidation);
+                boolean newPasswordCompare = this.passwordEncoder.matches(newPassword,userAlreadyExist.getPassword());
+                System.out.println("value of matches from newPassword" + currPasswordValidation);
+                System.out.println(newPasswordCompare);
+                if(!currPasswordValidation) {
+                    System.out.println("field old password here");
+                   resultValidation.put("fieldOldPassword","the Old password doesn't matches");
+                   return;
+                }
+                if(newPasswordCompare){
+                    System.out.println("field new password here");
+                    resultValidation.put("fieldNewPassword","the current password can't be the same to the current");
+                    return;
+                }
+
+                String patternValidatePassword = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,}$";
+                Pattern pattern = Pattern.compile(patternValidatePassword);
+                Matcher matcher = pattern.matcher(newPassword);
+
+                if(!matcher.matches()){
+                    resultValidation.put("fieldPassword","check the minimum requirements to update the fieldPassword");
+                    return;
+                }
+
+
+                String newEncryptedPassword = new BCryptPasswordEncoder().encode(newPassword);
+                userAlreadyExist.setPassword(newEncryptedPassword);
+                this.validateUpdate = true;
+                resultValidation.put("fieldPassword","updated success");
+                return;
+            }
+            resultValidation.put("fieldPassword","check the minimum requirements to update the fieldPassword");
+        }
+
+
+
+    }
+
+
+
+
+
+
+
