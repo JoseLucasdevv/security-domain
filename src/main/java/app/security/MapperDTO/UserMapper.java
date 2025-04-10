8aaedcb0ca3df39c3b32ecff9c5a5b5688ca11f5
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:53:59 -0300

    refactor: use uid instead id.

--- src/main/java/app/security/MapperDTO/UserMapper.java
+++ src/main/java/app/security/MapperDTO/UserMapper.java
@@ -8,9 +8,8 @@ public class UserMapper {
 
 
     public static UserDTO UserToDTO(User user){
-        return new UserDTO(user.getId(),user.getName(), user.getUsername(),user.getEmail(),user.getRole().getName(),user.getEmailConfirmed(),user.getWorkout().stream().toList());
+        return new UserDTO(user.getUid(),user.getName(), user.getUsername(),user.getEmail(),user.getRole().getName(),user.getEmailConfirmed(),user.getWorkout().stream().toList());
     }
-
 
     public static User DtoToUser(UserDTOIn user){
     User userEntity = new User();
@@ -18,10 +17,6 @@ public class UserMapper {
     userEntity.setUsername(user.username());
     userEntity.setEmail(user.email());
     userEntity.setPassword(user.password());
-
-
-
-
 
     return userEntity;
 
