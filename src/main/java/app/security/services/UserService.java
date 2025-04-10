5eba55290cfc4b438e53493129b290989675af41
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:54:48 -0300

    refactor: select using uid instead id and updateUser implement.

--- src/main/java/app/security/services/UserService.java
+++ src/main/java/app/security/services/UserService.java
@@ -3,24 +3,27 @@ package app.security.services;
 import app.security.Enum.TypeRole;
 import app.security.domain.User;
 import app.security.dto.UserDTO;
+import app.security.dto.UserDTOIn;
 import app.security.dto.UserUpdateDTO;
 
 
 import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
     //userConsumer
     void saveUser(User user);
 
 //    userTeacher
-    UserDTO getUserConsumerById(Long id);
-    UserDTO getUserById(Long id);
+    UserDTO getUserConsumerByUid(UUID uid);
+    UserDTO getUserByUid(UUID uid);
     List<UserDTO> getUsersConsumers(int pageNumber);
     List<UserDTO> getAllUsers(int pageNumber);
     UserDTO getUserByUsername(String username);
     UserDTO getUserByEmail(String email);
-    UserDTO deleteUser(Long id);
+    UserDTO deleteUser(UUID uid);
     void deleteUserByUsername(String username);
-    UserServiceImpl.ResponseUserUpdateValidationResource updateUser(Long id, UserUpdateDTO userUpdateDTO);
+    UserServiceImpl.ResponseUserUpdateValidationResource updateUserByUsername(String username, UserUpdateDTO userUpdateDTO);
+    UserServiceImpl.ResponseUserUpdateValidationResource updateUser(UUID uid, UserDTOIn userUpdateDTO);
     List<UserDTO> getAllUsersWithRole(TypeRole role, int pageNumber);
 }
