18ded77076d8ad237226267f67a528d18536eb60
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:48:06 -0300

    feat:create new methods to the EmailService

--- src/main/java/app/security/services/EmailService.java
+++ src/main/java/app/security/services/EmailService.java
@@ -2,5 +2,6 @@ package app.security.services;
 
 public interface EmailService {
     void verifyTokenEmailConfirmation(String token);
-
+    void sendVerificationCode(String username);
+    void verifyCode(String code);
 }
