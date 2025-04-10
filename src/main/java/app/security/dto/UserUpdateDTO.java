501d36d20def4eee00ae20bf911f864e39a14475
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:56:37 -0300

    feat:update properties from UserUpdateDTO

--- src/main/java/app/security/dto/UserUpdateDTO.java
+++ src/main/java/app/security/dto/UserUpdateDTO.java
@@ -1,4 +1,4 @@
 package app.security.dto;
 
-public record UserUpdateDTO(String name,String username,String email,String password ) {
+public record UserUpdateDTO(String name,String username,String oldPassword,String newPassword ) {
 }
