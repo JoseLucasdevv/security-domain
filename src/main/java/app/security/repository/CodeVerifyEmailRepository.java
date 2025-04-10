3888c1da7c6e02dc468cc91a63dc1ca634c6a3e0
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:47:47 -0300

    feat:create custom Query on CodeVerifyEmailRepository

--- src/main/java/app/security/repository/CodeVerifyEmailRepository.java
+++ src/main/java/app/security/repository/CodeVerifyEmailRepository.java
@@ -3,5 +3,10 @@ package app.security.repository;
 import app.security.domain.CodeVerifyEmail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CodeVerifyEmailRepository extends JpaRepository<CodeVerifyEmail,Long> {
+
+    Optional<CodeVerifyEmail> findCodeVerifyEmailByCode(String code);
+
 }
