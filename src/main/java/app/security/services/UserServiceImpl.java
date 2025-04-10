510fe7cee33cb2e2492bfd2cc20bcebfdc7ebf35
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:55:42 -0300

    feat:create method to updateUserByUsername and refactor to use uid.

--- src/main/java/app/security/services/UserServiceImpl.java
+++ src/main/java/app/security/services/UserServiceImpl.java
@@ -2,11 +2,13 @@ package app.security.services;
 import app.security.Enum.TypeRole;
 import app.security.MapperDTO.UserMapper;
 import app.security.domain.User;
+import app.security.dto.UserDTOIn;
 import app.security.dto.UserUpdateDTO;
 import app.security.exceptions.Exception;
 import app.security.repository.UserRepository;
 import app.security.dto.UserDTO;
 import app.security.services.validations.ResponseUserUpdateValidation;
+import app.security.services.validations.UserUpdateGeneralValidation;
 import app.security.services.validations.UserUpdateValidation;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +19,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Transactional
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
+
+    private final UserUpdateGeneralValidation userUpdateGeneralValidation;
     private final UserUpdateValidation userUpdateValidation;
     private final UserRepository userRepository;
 
@@ -34,9 +39,9 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO getUserConsumerById(Long id) {
+    public UserDTO getUserConsumerByUid(UUID uid) {
 
-        var user = this.userRepository.getUserById(TypeRole.USER,id);
+        var user = this.userRepository.getUserById(TypeRole.USER,uid);
         if(user == null) throw new Exception("Cannot find this user",HttpStatus.NOT_FOUND);
         log.info("get user {}",user.getUsername());
 
@@ -45,8 +50,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO getUserById(Long id) {
-        var user = this.userRepository.findById(id).orElseThrow(()-> new Exception("Can't find this user",HttpStatus.NOT_FOUND));
+    public UserDTO getUserByUid(UUID uid) {
+        var user = this.userRepository.findByUid(uid).orElseThrow(()-> new Exception("Can't find this user",HttpStatus.NOT_FOUND));
         log.info("get user {}",user.getUsername());
         return UserMapper.UserToDTO(user);
 
@@ -93,8 +98,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO deleteUser(Long id) {
-        User usr = this.userRepository.findById(id).orElseThrow(()-> new Exception("Can't find this user", HttpStatus.NOT_FOUND));
+    public UserDTO deleteUser(UUID uid) {
+        User usr = this.userRepository.findByUid(uid).orElseThrow(()-> new Exception("Can't find this user", HttpStatus.NOT_FOUND));
         this.userRepository.delete(usr);
         return UserMapper.UserToDTO(usr);
     }
@@ -105,9 +110,10 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public ResponseUserUpdateValidationResource updateUser(Long id, UserUpdateDTO userUpdateDTO){
+    public ResponseUserUpdateValidationResource updateUser(UUID uid, UserDTOIn userUpdateDTO){
 
-        User user = this.userRepository.findById(id).orElseThrow(()-> new Exception("this user doesn't exists", HttpStatus.NOT_FOUND));
+
+        User user = this.userRepository.findByUid(uid).orElseThrow(()-> new Exception("this user doesn't exists", HttpStatus.NOT_FOUND));
 
         ResponseUserUpdateValidation userUpdated = this.userUpdateValidation.validationUpdate(user,userUpdateDTO);
 
@@ -115,6 +121,14 @@ public class UserServiceImpl implements UserService {
 
         return new ResponseUserUpdateValidationResource(UserMapper.UserToDTO(userUpdated.user()), userUpdated.resultValidation());
 
+
+    }
+    @Override
+    public ResponseUserUpdateValidationResource updateUserByUsername(String username,UserUpdateDTO userUpdateDTO) {
+        User user = this.userRepository.findByUsername(username);
+        ResponseUserUpdateValidation userUpdated = this.userUpdateGeneralValidation.validationUpdate(user,userUpdateDTO);
+        this.userRepository.save(userUpdated.user());
+        return new ResponseUserUpdateValidationResource(UserMapper.UserToDTO(userUpdated.user()), userUpdated.resultValidation());
 
     }
 
