ddff2a5236cd18215db824e00a90a12429adb1c8
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:56:55 -0300

    refactor: UserUpdateValidation

--- src/main/java/app/security/services/validations/UserUpdateValidation.java
+++ src/main/java/app/security/services/validations/UserUpdateValidation.java
@@ -1,6 +1,7 @@
 package app.security.services.validations;
 
 import app.security.domain.User;
+import app.security.dto.UserDTOIn;
 import app.security.dto.UserUpdateDTO;
 
 import app.security.exceptions.Exception;
@@ -8,6 +9,7 @@ import app.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,23 +24,24 @@ public class UserUpdateValidation {
 public final HashMap<String, String> resultValidation = new HashMap<String, String>();
 private final UserRepository userRepository;
 private boolean validateUpdate = false;
-    public ResponseUserUpdateValidation validationUpdate(User userAlreadyExist, UserUpdateDTO userUpdateValidation){
-        this.validateName(userAlreadyExist,userUpdateValidation);
-        this.validateUsername(userAlreadyExist,userUpdateValidation);
-        this.validateEmail(userAlreadyExist,userUpdateValidation);
-        this.validatePassword(userAlreadyExist,userUpdateValidation);
+    public ResponseUserUpdateValidation validationUpdate(User userAlreadyExist, UserDTOIn userUpdateValidation){
+        resultValidation.clear();
+        this.validateName(userAlreadyExist,userUpdateValidation.name());
+        this.validateUsername(userAlreadyExist,userUpdateValidation.username());
+        this.validateEmail(userAlreadyExist,userUpdateValidation.email());
+        this.validatePassword(userAlreadyExist,userUpdateValidation.password());
 
-        if(!this.validateUpdate) throw new Exception("Nothing to update", HttpStatus.OK);
+        if(!this.validateUpdate) throw new Exception("Nothing to update", HttpStatus.BAD_REQUEST);
 
         userAlreadyExist.setUpdatedAt(LocalDateTime.now());
         resultValidation.put("updated",userAlreadyExist.getUpdatedAt().toString());
         return new ResponseUserUpdateValidation(userAlreadyExist,this.resultValidation);
     }
 
-    private void validateName(User userAlreadyExist, UserUpdateDTO userUpdateValidation){
+    private void validateName(User userAlreadyExist, String name){
 
-        if(userUpdateValidation.name() != null && !userUpdateValidation.name().isBlank() && userUpdateValidation.name().length() >= 3 && userUpdateValidation.name().length() <= 55){
-            userAlreadyExist.setName(userUpdateValidation.name().trim());
+        if(name != null && !name.isBlank() && name.length() >= 3 && name.length() <= 55){
+            userAlreadyExist.setName(name.trim());
             resultValidation.put("fieldName","Update Success");
             this.validateUpdate = true;
             return;
@@ -47,17 +50,17 @@ private boolean validateUpdate = false;
         resultValidation.put("fieldName","check the minimum requirements to update the FieldName");
     }
 
-    private void validateUsername(User userAlreadyExist, UserUpdateDTO userUpdateValidation) {
+    private void validateUsername(User userAlreadyExist, String username) {
 
-        if(userUpdateValidation.username() != null && !userUpdateValidation.username().isBlank() && userUpdateValidation.username().length() >= 3 && userUpdateValidation.username().length() <= 55){
-            User userFromDB = this.userRepository.findByUsername(userUpdateValidation.username().trim());
+        if(username != null && !username.isBlank() && username.length() >= 3 && username.length() <= 55){
+            User userFromDB = this.userRepository.findByUsername(username.trim());
 
             if(userFromDB != null) {
                 resultValidation.put("fieldUsername","this username isn't allowed");
                 return;
             }
 
-            userAlreadyExist.setUsername(userUpdateValidation.username().trim());
+            userAlreadyExist.setUsername(username.trim());
             resultValidation.put("fieldUsername","Update Success");
             this.validateUpdate = true;
             return;
@@ -65,16 +68,16 @@ private boolean validateUpdate = false;
         resultValidation.put("fieldUsername","check the minimum requirements to update the FieldUsername");
     }
 
-    private void validateEmail(User userAlreadyExist, UserUpdateDTO userUpdateValidation){
-        if(userUpdateValidation.email() != null && !userUpdateValidation.name().isBlank()){
-            User usrFromDB = this.userRepository.findByEmail(userUpdateValidation.email());
+    private void validateEmail(User userAlreadyExist, String email){
+        if(email != null && !email.isBlank()){
+            User usrFromDB = this.userRepository.findByEmail(email);
             if(usrFromDB != null){
                 this.resultValidation.put("fieldEmail","this email have been already used from another user.");
                 return;
             }
 
 
-            userAlreadyExist.setEmail(userUpdateValidation.email());
+            userAlreadyExist.setEmail(email);
             resultValidation.put("fieldEmail","Updated Success");
             this.validateUpdate = true;
             return;
@@ -84,17 +87,18 @@ private boolean validateUpdate = false;
 
     }
 
-    private void validatePassword(User userAlreadyExist, UserUpdateDTO userUpdateValidation){
-        if(userUpdateValidation.password() != null && !userUpdateValidation.password().isBlank()){
+    private void validatePassword(User userAlreadyExist, String password){
+        if(password != null && !password.isBlank()){
             String patternValidatePassword = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,}$";
             Pattern pattern = Pattern.compile(patternValidatePassword);
-            Matcher matcher = pattern.matcher(userUpdateValidation.password());
+            Matcher matcher = pattern.matcher(password);
 
         if(!matcher.matches()){
             resultValidation.put("fieldPassword","check the minimum requirements to update the fieldPassword");
             return;
         }
-        String newEncryptedPassword = new BCryptPasswordEncoder().encode(userUpdateValidation.password());
+
+        String newEncryptedPassword = new BCryptPasswordEncoder().encode(password);
         userAlreadyExist.setPassword(newEncryptedPassword);
         this.validateUpdate = true;
         resultValidation.put("fieldPassword","updated success");
