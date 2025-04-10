3bc7d40a5649a507372328b722db673aefe7a43e
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:56:04 -0300

    refactor: use uid instead id .

--- src/main/java/app/security/api/UserTeacherResource.java
+++ src/main/java/app/security/api/UserTeacherResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -37,10 +38,10 @@ public class UserTeacherResource {
         }
     }
 
-    @GetMapping("/teacher/user/{id}")
-    public ResponseEntity<?> getUser(@PathVariable Long id){
+    @GetMapping("/teacher/user/{uid}")
+    public ResponseEntity<?> getUser(@PathVariable String uid){
        try{
-           return ResponseEntity.ok().body(userService.getUserConsumerById(id));
+           return ResponseEntity.ok().body(userService.getUserConsumerByUid(UUID.fromString(uid)));
        }catch(Exception e){
                 HashMap<String,String> objectHashError = HashError.createHashErrorOutput(e.getMessage());
            return ResponseEntity.badRequest().body(objectHashError);
@@ -48,12 +49,12 @@ public class UserTeacherResource {
 
     }
 
-    @PostMapping("/teacher/workout/{userId}")
-    public ResponseEntity<?> saveWorkout(@PathVariable Long userId,@RequestBody @Valid WorkoutCreateDTO workout){
+    @PostMapping("/teacher/workout/{userUid}")
+    public ResponseEntity<?> saveWorkout(@PathVariable String userUid,@RequestBody @Valid WorkoutCreateDTO workout){
         try{
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String extractUsername = authentication.getName();
-        return ResponseEntity.status(201).body(this.workoutService.createWorkout(userId,workout,extractUsername));
+        return ResponseEntity.status(201).body(this.workoutService.createWorkout(UUID.fromString(userUid),workout,extractUsername));
         }catch(Exception e){
             HashMap<String,String> objectHashError = HashError.createHashErrorOutput(e.getMessage());
             return ResponseEntity.badRequest().body(objectHashError);
@@ -61,12 +62,12 @@ public class UserTeacherResource {
         }
     }
 
-    @PutMapping("/teacher/workout/{userId}/{workoutId}")
-    public ResponseEntity<?> updateWorkout(@PathVariable Long userId,@PathVariable Long workoutId,@RequestBody WorkoutUpdateDTO workout){
+    @PutMapping("/teacher/workout/{userUid}/{workoutId}")
+    public ResponseEntity<?> updateWorkout(@PathVariable String userUid,@PathVariable Long workoutId,@RequestBody WorkoutUpdateDTO workout){
         try{
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String extractUsername = authentication.getName();
-        return ResponseEntity.status(201).body(this.workoutService.updateWorkout(workoutId,userId,workout,extractUsername));
+        return ResponseEntity.status(201).body(this.workoutService.updateWorkout(workoutId,UUID.fromString(userUid),workout,extractUsername));
         }catch(Exception e){
             HashMap<String,String> errors = HashError.createHashErrorOutput(e.getMessage());
             return ResponseEntity.badRequest().body(errors);
@@ -95,7 +96,7 @@ public class UserTeacherResource {
     }
 
     @DeleteMapping("/teacher/workout/{userId}/{workoutId}")
-    public ResponseEntity<?> deleteWorkout(@PathVariable Long userId,@PathVariable Long workoutId){
+    public ResponseEntity<?> deleteWorkout(@PathVariable UUID userId,@PathVariable Long workoutId){
         try {
             this.workoutService.deleteWorkoutById(userId, workoutId);
             return ResponseEntity.ok().build();
