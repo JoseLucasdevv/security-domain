44bbaf5e5a74b4a6e4d834cc7b523875d9fca6ba
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-10 10:57:47 -0300

    refactor: use uid instead id.

--- src/main/java/app/security/services/WorkoutService.java
+++ src/main/java/app/security/services/WorkoutService.java
@@ -6,14 +6,15 @@ import app.security.dto.WorkoutDTO;
 import app.security.dto.WorkoutUpdateDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface WorkoutService {
 
     List<WorkoutDTO<String>> getAllWorkoutFromUser(String username);
     WorkoutDTO<String> getSpecificWorkoutFromUser(String username , Long workoutId);
-    void deleteWorkoutById(Long userId, Long workoutId);
-    UserDTO createWorkout(Long userId, WorkoutCreateDTO workout, String nameOfTeacher);
-    UserDTO updateWorkout(Long workoutId, Long userId, WorkoutUpdateDTO workout, String nameOfTeacher);
+    void deleteWorkoutById(UUID userUid, Long workoutId);
+    UserDTO createWorkout(UUID userUid, WorkoutCreateDTO workout, String nameOfTeacher);
+    UserDTO updateWorkout(Long workoutId, UUID userUid, WorkoutUpdateDTO workout, String nameOfTeacher);
 
 
 }
