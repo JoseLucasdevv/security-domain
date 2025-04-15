5214663b8c01b23fb8075c488aa6d9ce1ca43eab
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-15 09:31:52 -0300

    feat:create setNewEmailToken domain class.

---
+++ src/main/java/app/security/domain/SetNewEmailToken.java
@@ -0,0 +1,44 @@
+package app.security.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class SetNewEmailToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private String newEmail;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+}
