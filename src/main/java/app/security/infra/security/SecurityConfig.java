0d857b0b7e74f51ddddab62f404115639ba45817
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-15 09:34:47 -0300

    refactor:format the endpoints using lead Slash.

--- src/main/java/app/security/infra/security/SecurityConfig.java
+++ src/main/java/app/security/infra/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package app.security.infra.security;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -33,39 +33,43 @@ public class SecurityConfig{
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.
                         // auth Users
-                        requestMatchers(HttpMethod.POST,"api/register").permitAll().
-                        requestMatchers(HttpMethod.POST,"api/auth").permitAll().
-                        requestMatchers(HttpMethod.POST,"api/refresh_token").permitAll().
-                        requestMatchers(HttpMethod.GET,"api/confirm-email").permitAll().
-                        requestMatchers(HttpMethod.POST,"api/forgot-password").permitAll()
-                        .requestMatchers(HttpMethod.POST,"api/reset-password").permitAll()
-
+                        requestMatchers(HttpMethod.POST,"/api/register").permitAll().
+                        requestMatchers(HttpMethod.POST,"/api/auth").permitAll().
+                        requestMatchers(HttpMethod.POST,"/api/refresh_token").permitAll().
+                        requestMatchers(HttpMethod.GET,"/api/confirm-email").permitAll().
+                        requestMatchers(HttpMethod.POST,"/api/forgot-password").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/reset-password").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/user/change-email/**").permitAll()
 
                         //Just authenticated.
-                        .requestMatchers(HttpMethod.GET,"api/resend-email").authenticated().
-                        requestMatchers(HttpMethod.POST,"api/logOut").authenticated()
-                        .requestMatchers(HttpMethod.DELETE,"api/user").authenticated()
-                        .requestMatchers(HttpMethod.PATCH,"api/user").authenticated()
-                        .requestMatchers(HttpMethod.GET,"api/user/send-code").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/api/resend-email").authenticated().
+                        requestMatchers(HttpMethod.POST,"/api/logOut").authenticated()
+                        .requestMatchers(HttpMethod.DELETE,"/api/user").authenticated()
+                        .requestMatchers(HttpMethod.PATCH,"/api/user").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/api/user/send-code").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/api/user/verify-code/**").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/api/user/update-email/**").authenticated()
+
+
                         // TeacherConsumer
-                        .requestMatchers(HttpMethod.GET,"api/teacher/users/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.GET,"api/teacher/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.POST,"api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.PUT,"api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.GET,"api/teacher/workouts/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.GET,"api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
-                        .requestMatchers(HttpMethod.DELETE,"api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.GET,"/api/teacher/users/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.GET,"/api/teacher/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.POST,"/api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.PUT,"/api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.GET,"/api/teacher/workouts/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.GET,"/api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
+                        .requestMatchers(HttpMethod.DELETE,"/api/teacher/workout/**").access(authorizationManagerFactory.emailConfirmedAndRole("TEACHER"))
 
                         //User Consumer
-                        .requestMatchers(HttpMethod.GET,"api/user").access(authorizationManagerFactory.emailConfirmedAndRole("USER"))
+                        .requestMatchers(HttpMethod.GET,"/api/user").access(authorizationManagerFactory.emailConfirmedAndRole("USER"))
 
 
                         //User Admin
-                        .requestMatchers(HttpMethod.POST,"api/admin/register-teacher").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
-                        .requestMatchers(HttpMethod.GET,"api/admin/users/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
-                        .requestMatchers(HttpMethod.GET,"api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
-                        .requestMatchers(HttpMethod.DELETE,"api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
-                        .requestMatchers(HttpMethod.PUT,"api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
+                        .requestMatchers(HttpMethod.POST,"/api/admin/register-teacher").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
+                        .requestMatchers(HttpMethod.GET,"/api/admin/users/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
+                        .requestMatchers(HttpMethod.GET,"/api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
+                        .requestMatchers(HttpMethod.DELETE,"/api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
+                        .requestMatchers(HttpMethod.PUT,"/api/admin/user/**").access(authorizationManagerFactory.emailConfirmedAndRole("ADMIN"))
 
 
                 ).addFilterBefore(filterValidateJWT, UsernamePasswordAuthenticationFilter.class);
