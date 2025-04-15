d9ca43d1523c2126481ec69dd20c26e344d2cd97
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-15 09:34:08 -0300

    feat:make implementation methods for EmailServiceImpl --> verifyCode and emailSenderChangeEmail

--- src/main/java/app/security/services/EmailServiceImpl.java
+++ src/main/java/app/security/services/EmailServiceImpl.java
@@ -1,22 +1,31 @@
 package app.security.services;
 
+import app.security.domain.ChangeEmailToken;
 import app.security.domain.CodeVerifyEmail;
+import app.security.domain.SetNewEmailToken;
 import app.security.domain.User;
+import app.security.dto.ChangeEmailDTO;
 import app.security.exceptions.Exception;
 import app.security.mailProvider.SendEmailService;
-import app.security.repository.CodeVerifyEmailRepository;
-import app.security.repository.EmailConfirmationTokenRepository;
-import app.security.repository.UserRepository;
+import app.security.repository.*;
 import app.security.utils.GenerateCodeVerification;
 import app.security.utils.GenerateExpirationDate;
+import app.security.utils.GenerateKeyEncoded;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private SetNewEmailTokenRepository setNewEmailTokenRepository;
+    @Autowired
+    private ChangeEmailTokenRepository  changeEmailTokenRepository;
     @Autowired
     private SendEmailService sendEmailService;
     @Autowired
@@ -52,9 +61,52 @@ public class EmailServiceImpl implements EmailService {
 
     }
     @Override
-    public void verifyCode(String code) {
+    public ChangeEmailToken verifyCode(String code, String username) {
+        // use CodeVerifyEmailRepository instead UserRepository
+
+        User user = this.userRepository.findByUsername(username);
+        CodeVerifyEmail codeVerifyEmail = user.getCodeVerifyEmail()
+                .stream()
+                .filter(entity -> entity.getCode().equals(code))
+                .findFirst()
+                .orElseThrow(() -> new Exception("we can't find this token",HttpStatus.NOT_FOUND));
+        if(codeVerifyEmail.getExpiresAt().isBefore(Instant.now())) throw new Exception("this code has already expired",HttpStatus.NOT_ACCEPTABLE);
+        user.getCodeVerifyEmail().clear();
+        ChangeEmailToken changeEmailToken = new ChangeEmailToken();
+        String token = GenerateKeyEncoded.getKeyEncodedToken();
+        changeEmailToken.setToken(token);
+        changeEmailToken.setUser(user);
+        changeEmailToken.setExpiresAt(GenerateExpirationDate.genExpirationDate(20L));
+        changeEmailTokenRepository.save(changeEmailToken);
+        // create key token to send to the user.
+        return changeEmailToken;
+    }
+
+    @Override
+    public void emailSenderChangeEmail(String token, ChangeEmailDTO contentBody) {
+     ChangeEmailToken changeEmailToken = this.changeEmailTokenRepository.
+             findByToken(token).
+             orElseThrow(() -> new Exception("Oops, Something wrong !",HttpStatus.NOT_FOUND));
+
+            if(changeEmailToken.getExpiresAt().isBefore(Instant.now())) throw new Exception("the token was expired",HttpStatus.BAD_REQUEST);
+            User user = changeEmailToken.getUser();
+
+            User userFound = this.userRepository.findByEmail(contentBody.newEmail());
+            if(userFound != null) throw new Exception("there is User that already use this email",HttpStatus.FOUND);
+
+            boolean matcher = this.passwordEncoder.matches(contentBody.password(), user.getPassword());
 
 
-        return null;
+            if(!matcher) throw new Exception("your password isn't correct",HttpStatus.NOT_FOUND);
+            SetNewEmailToken setNewEmailToken = new SetNewEmailToken();
+            String tokenSetEmail = GenerateKeyEncoded.getKeyEncodedToken();
+            setNewEmailToken.setToken(tokenSetEmail);
+            setNewEmailToken.setUser(user);
+            setNewEmailToken.setExpiresAt(GenerateExpirationDate.genExpirationDate(1L));
+            setNewEmailToken.setNewEmail(contentBody.newEmail());
+            setNewEmailTokenRepository.save(setNewEmailToken);
+            this.sendEmailService.sendChangeEmailUpdate(setNewEmailToken);
+
+
     }
 }
