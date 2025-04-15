ac6cc6be01900f67842f17e41355388149dd651b
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-15 09:35:20 -0300

    feat: create new method sendChangeEmailUpdate

--- src/main/java/app/security/mailProvider/SendEmailService.java
+++ src/main/java/app/security/mailProvider/SendEmailService.java
@@ -1,8 +1,11 @@
 package app.security.mailProvider;
 
+import app.security.domain.SetNewEmailToken;
+
 public interface SendEmailService {
     void sendConfirmationLink(String token);
     void sendForgotPasswordLink(String token);
     void SendCodeVerifyEmail(String code);
+    void sendChangeEmailUpdate(SetNewEmailToken setNewEmailToken);
 
 }
